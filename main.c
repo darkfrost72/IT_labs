6dcb87e3e63e342f34f6ffca454e3bcbf454089c
Vlad Nikitin <nikitin.vladyslav1@lll.kpi.ua>
2021-10-29 15:31:17 +0300

    Added locales and goto to avoid pick variant c>3 and c=0

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include<locale.h>
 #include <stdbool.h>
 
 double funct(float x)
@@ -17,67 +18,77 @@ void head()
 
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     unsigned int choice = 1, n;
-
     float x1, x2, delta;
     int i;
-
+    restart:
     printf("Выбери вариант 1 или 2\n");
     scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            printf("Введите данные.\n");
-            printf("X1\n");
-            scanf("%f", &x1);
-            printf("X2\n");
-            scanf("%f", &x2);
-            printf("Количество строк\n");
-            scanf("%d", &n);
-            float X=x1;
-            printf("x1=%lf  x2=%lf количество строк=%d\n", x1, x2, n);
-            head();
-            i=1;
-            while (X<=x2 && i <= n) {
-                delta = (x2 - X) / (n - 1);
-                printf("|");
-                printf("----%d----", i);
-                printf("|");
-                printf("%lf",X);
-                X+=delta;
-                X++;
-                printf("|");
-                printf("%lf", funct(X));
-                printf("|\n");
-                i++;
-            }
-            printf("+---------+---------+---------+\n");
-            break;
+    if (choice > 2) {
+        printf("Вас просят выбрать значение либо 1, либо 2. Повторите попытку, пожалуйста\n");
+        goto restart;
+    }
+    if (choice == 0) {
+        printf("Вас просят выбрать значение либо 1, либо 2. Повторите попытку, пожалуйста\n");
+        goto restart;
+    }
+    else {
+        switch (choice) {
+            case 1:
+                printf("Введите данные.\n");
+                printf("X1\n");
+                scanf("%f", &x1);
+                printf("X2\n");
+                scanf("%f", &x2);
+                printf("Количество строк\n");
+                scanf("%d", &n);
+                float X=x1;
+                printf("x1=%lf  x2=%lf количество строк=%d\n", x1, x2, n);
+                head();
+                i=1;
+                while (X<=x2 && i <= n) {
+                    delta = (x2 - X) / (n - 1);
+                    printf("|");
+                    printf("----%d----", i);
+                    printf("|");
+                    printf("%lf",X);
+                    X+=delta;
+                    X++;
+                    printf("|");
+                    printf("%lf", funct(X));
+                    printf("|\n");
+                    i++;
+                }
+                printf("+---------+---------+---------+\n");
+                break;
 
-        case 2:
-            printf("Введите данные.\n");
-            printf("X1\n");
-            scanf("%f", &x1);
-            printf("X2\n");
-            scanf("%f", &x2);
-            printf("Дельта\n");
-            scanf("%f", &delta);
-            printf("x1=%f  x2=%f Дельта=%f\n", x1, x2, delta);
-            head();
-            i=1;
-            while (X<=x2 && i <= 2) {
-                printf("|");
-                printf("----%d----", i);
-                printf("|");
-                printf("%f",x1);
-                x1+=delta;
-                x1++;
-                printf("|");
-                printf("%f", funct(x1));
-                printf("|\n");
-                i++;
-            }
-            printf("+---------+---------+---------+\n");
-            break;
+            case 2:
+                printf("Введите данные.\n");
+                printf("X1\n");
+                scanf("%f", &x1);
+                printf("X2\n");
+                scanf("%f", &x2);
+                printf("Дельта\n");
+                scanf("%f", &delta);
+                printf("x1=%f  x2=%f Дельта=%f\n", x1, x2, delta);
+                head();
+                i=1;
+                while (X<=x2 && i <= 2) {
+                    printf("|");
+                    printf("----%d----", i);
+                    printf("|");
+                    printf("%f",x1);
+                    x1+=delta;
+                    x1++;
+                    printf("|");
+                    printf("%f", funct(x1));
+                    printf("|\n");
+                    i++;
+                }
+                printf("+---------+---------+---------+\n");
+                break;
+        }
     }
     return 0;
 }
